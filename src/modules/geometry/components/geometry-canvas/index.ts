396cc88b818e117b9c9dc8fe6962815f34c5a595
Gabriel Ramos <whosramoss@gmail.com>
2025-07-26 15:11:21 -0300

    chore: add component GeometryCanvas

---
+++ src/modules/geometry/components/geometry-canvas/index.ts
@@ -0,0 +1,11 @@
+import GeometryCanvasComponent from "src/modules/geometry/components/geometry-canvas/GeometryCanvasComponent";
+import GeometryCanvasConfig from "src/modules/geometry/components/geometry-canvas/GeometryCanvasConfig";
+import GeometryCanvasLoader from "src/modules/geometry/components/geometry-canvas/GeometryCanvasLoader";
+import GeometryCanvasRoot from "src/modules/geometry/components/geometry-canvas/GeometryCanvasRoot";
+
+export const GeometryCanvas = {
+  Component: GeometryCanvasComponent,
+  Config: GeometryCanvasConfig,
+  Loader: GeometryCanvasLoader,
+  Root: GeometryCanvasRoot,
+};
