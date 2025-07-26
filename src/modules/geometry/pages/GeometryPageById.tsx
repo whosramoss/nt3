6f1a531723f120616a2848fe3d0229f69affbe23
Gabriel Ramos <whosramoss@gmail.com>
2025-07-26 15:50:27 -0300

    chore: add pages of Geometry module

---
+++ src/modules/geometry/pages/GeometryPageById.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import { GeometryCanvas } from "src/modules/geometry/components/geometry-canvas";
+
+interface GeometryByIdProps {
+  id: string;
+}
+
+export default function GeometryByIdPage({ id }: GeometryByIdProps) {
+  return (
+    <GeometryCanvas.Root>
+      <GeometryCanvas.Loader>
+        <GeometryCanvas.Config>
+          <GeometryCanvas.Component id={id} />
+        </GeometryCanvas.Config>
+      </GeometryCanvas.Loader>
+    </GeometryCanvas.Root>
+  );
+}
